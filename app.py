444fcfa733858e18d3d4321b306ff42925341dae
Mike <80673026+mssj-11@users.noreply.github.com>
2023-06-25 22:48:15 -0600

    Python

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
 from routes.character import character
+from config.openapi import tags_metadata
 
-app = FastAPI()
-
+app = FastAPI(
+    title="CDZ API",
+    description="A REST API using FastAPI and MySQL",
+    version="0.0.1",
+    openapi_tags=tags_metadata,
+)
 app.include_router(character)
