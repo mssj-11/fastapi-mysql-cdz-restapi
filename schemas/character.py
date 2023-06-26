b400cfea508096fee4015fc168a9c28a943c7bef
Mike <80673026+mssj-11@users.noreply.github.com>
2023-06-25 22:56:05 -0600

    Esquemas

--- schemas/character.py
+++ schemas/character.py
@@ -1,10 +1,15 @@
 from typing import Optional
 from pydantic import BaseModel
 
+
 class Character(BaseModel):
-    id: Optional[str]
+    id: Optional[int]
     name: str
     constellation: str
     rank: str
     description: str
     image_url: str
+
+
+class CharacterCount(BaseModel):
+    total: int
