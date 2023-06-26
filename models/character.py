3520c8245972451845c2553c6547d5eb44f02961
Mike <80673026+mssj-11@users.noreply.github.com>
2023-06-25 22:54:16 -0600

    Modelos

--- models/character.py
+++ models/character.py
@@ -3,7 +3,16 @@ from sqlalchemy.sql.sqltypes import Integer, String
 from config.db import meta, engine
 
 
-characters = Table("characters", meta, Column("id", Integer, primary_key=True), Column("name", String(255)), Column("constellation", String(100)), Column("rank", String(50)), Column("description", String(255)), Column("image_url", String(255)))
+characters = Table(
+    "characters", 
+    meta, 
+    Column("id", Integer, primary_key=True),
+    Column("name", String(255)),
+    Column("constellation", String(255)),
+    Column("rank", String(255)),
+    Column("description", String(255)),
+    Column("image_url", String(255)),
+)
 
 
 meta.create_all(engine)
