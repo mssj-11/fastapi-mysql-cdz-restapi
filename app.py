04830145dab2881754fa6427fb626e3af164e009
Mike <80673026+mssj-11@users.noreply.github.com>
2023-06-26 18:04:51 -0600

    Python

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routes.character import character
 from config.openapi import tags_metadata
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="CDZ API",
@@ -8,4 +10,16 @@ app = FastAPI(
     version="0.0.1",
     openapi_tags=tags_metadata,
 )
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(character)
